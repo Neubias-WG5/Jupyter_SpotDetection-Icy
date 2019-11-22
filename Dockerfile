90e8cf2f0fb2e2fbeafe0b3c0bc2baa10712999b
volker-baecker <volker.baecker@mri.cnrs.fr>
2019-11-22 15:48:24 +0100

    first working version

---
+++ Dockerfile
@@ -0,0 +1,60 @@
+FROM jupyter/minimal-notebook
+# ---------------------------------------------------------------------------------------------------------------------
+USER root
+
+RUN apt-get update && apt-get install libgeos-dev -y && apt-get clean
+# ---------------------------------------------------------------------------------------------------------------------
+# Install Java
+RUN apt-get update && apt-get install openjdk-8-jdk -y && apt-get clean
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Install Cytomine python client
+RUN cd / && git clone https://github.com/cytomine-uliege/Cytomine-python-client.git
+RUN cd /Cytomine-python-client && git checkout tags/v2.4.0 && pip install .
+RUN rm -r /Cytomine-python-client
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Install Icy.
+RUN apt-get update && apt-get install -y unzip wget && \
+    mkdir -p /icy && \
+    cd /icy && \
+    wget -O icy.zip https://github.com/Neubias-WG5/W_SpotDetection-Icy/raw/master/icy_1.9.9.1_with_plugins.zip && \
+    unzip icy.zip && \
+    rm -rf icy.zip
+
+# Add icy to the PATH
+ENV PATH $PATH:/icy
+
+RUN mkdir -p /icy/data/in && \
+        mkdir -p /icy/protocols
+
+RUN chmod -R a+rwx /icy 
+# ---------------------------------------------------------------------------------------------------------------------
+# Install Neubias-W5-Utilities (annotation exporter, compute metrics, helpers,...)
+RUN cd / && git clone https://github.com/Neubias-WG5/neubiaswg5-utilities.git && \
+       cd /neubiaswg5-utilities/ && git checkout tags/v0.8.2 && pip install .
+
+# install utilities binaries
+RUN chmod +x /neubiaswg5-utilities/bin/*
+RUN cp /neubiaswg5-utilities/bin/* /usr/bin/
+
+# cleaning
+RUN rm -r /neubiaswg5-utilities
+
+# ---------------------------------------------------------------------------------------------------------------------
+# Install Protocol
+ADD protocol.protocol /icy/protocols/protocol.protocol
+ADD wrapper.py /app/wrapper.py
+
+# for running the wrapper locally
+ADD descriptor.json /app/descriptor.json
+
+# changing access rights to the app folder
+RUN chmod -R a+rx /app
+RUN chmod -R a+rw /icy
+
+USER ${NB_USER}
+COPY . ${HOME}
+USER root
+RUN chown -R ${NB_UID} ${HOME}
+USER ${NB_USER}
---
+++ wrapper.py
@@ -0,0 +1,60 @@
+import os
+import sys
+from cytomine.models import *
+from subprocess import call
+from pathlib import Path
+
+from neubiaswg5 import CLASS_SPTCNT
+from neubiaswg5.helpers import prepare_data, NeubiasJob, upload_data, upload_metrics, get_discipline
+
+
+def makedirs(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+
+def clean_icy_result_file(filepath, n=1):
+    lines = None
+    with open(filepath, "r") as file:
+        lines = file.readlines()
+    with open(filepath, "w") as file:
+        file.writelines([l for l in lines[n:] if len(l.strip()) > 0])
+
+
+def main():
+    with NeubiasJob.from_cli(sys.argv[1:]) as nj:
+        nj.job.update(status=Job.RUNNING, progress=0, statusComment="Initialization...")
+
+        # 1. Create working directories on the machine:
+        # 2. Download (or read) data
+        problem_cls = get_discipline(nj, default=CLASS_SPTCNT)
+        is_2d = True
+        nj.job.update(progress=1, statusComment="Execute workflow on problem class '{}'".format(problem_cls))
+        in_images, gt_images, in_path, gt_path, out_path, tmp_path = prepare_data(problem_cls, nj, is_2d=is_2d, **nj.flags)
+
+        # 3. Execute workflow
+        scale3sens = nj.parameters.icy_scale3sensitivity
+        nj.job.update(progress=25, statusComment="Launching workflow...")
+        call("java -cp /icy/lib/ -jar /icy/icy.jar -hl", shell=True, cwd="/icy")
+        call("java -cp /icy/lib/ -jar /icy/icy.jar -hl -x plugins.adufour.protocols.Protocols "
+             "protocol=\"/icy/protocols/protocol.protocol\" inputFolder=\"{}\" outputFolder=\"{}\" extension=tif "
+             "scale2enable=true scale2sensitivity={}".format(in_path, out_path, scale3sens), shell=True, cwd="/icy")
+
+        # 3.5 Remove the xml-output files
+        for p in Path(out_path).glob("*.xml"):
+            p.unlink()
+        
+        # 4. Upload the annotation and labels to Cytomine
+        upload_data(problem_cls, nj, in_images, out_path, **nj.flags, is_2d=is_2d, monitor_params={
+           "start": 60, "end": 90, "period": 0.1
+        })
+
+        # 5. Compute and upload the metrics
+        nj.job.update(progress=90, statusComment="Computing and uploading metrics...")
+        upload_metrics(problem_cls, nj, in_images, gt_path, out_path, tmp_path, **nj.flags)
+
+        nj.job.update(progress=100, status=Job.TERMINATED, status_comment="Finished.")
+
+
+if __name__ == "__main__":
+    main()
